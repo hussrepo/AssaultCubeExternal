ddeae84c13e5b60a9fde7951d77a754f58756984
hussrepo <177785889+hussrepo@users.noreply.github.com>
2025-06-17 15:19:54 -0400

    Initial commit

---
+++ AssaultCubeExternal/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace AssaultCubeExternal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Health";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ammo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Armor";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(74, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Set";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 24);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Set";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(146, 144);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(252, 32);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Unlimited";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(252, 89);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(78, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Unlimited";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(252, 148);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(78, 17);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "Unlimited";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Permisson Status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(108, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Premium";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(220, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Menu Status:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Lime;
+            this.label7.Location = new System.Drawing.Point(301, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Online";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(273, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Connect";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 3);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "External Menu V2";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(360, 185);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Assault Cube External Menu V2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+    }
+}
+
---
+++ AssaultCubeExternal/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssaultCubeExternal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int Base = 0x00509B74;
+        public static int Health = 0xF8;
+        public static int GrenadeAmmo = 0x158;
+        public static int Armor = 0xFC;
+        public static int Ammo = 0x150;
+        VAMemory vam = new VAMemory("ac_client");
+        public static Process[] check = Process.GetProcessesByName("ac_client");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("INTENDED ONLY FOR OFFLINE PLAY. USING THIS IN ONLINE MODE VIOLATES TERMS OF SERVICE.");
+            VAMemory vam = new VAMemory("ac_client");
+            timer3.Start();
+            timer1.Interval = 1000;
+            timer2.Interval = 500;
+            timer4.Start();
+            label7.Text = "Offline";
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            checkBox1.Enabled = false;
+            checkBox2.Enabled = false;
+            checkBox3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int LocalPlayer = vam.ReadInt32((IntPtr)0x00509B74);
+            int address = LocalPlayer + Health;
+            int amount = int.Parse(textBox1.Text);
+            vam.WriteInt32((IntPtr)address, amount);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (check.Length == 0)
+            {
+                label7.Text = "Offline";
+                timer2.Stop();
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+                checkBox3.Enabled = false;
+                button4.Show();
+                timer1.Stop();
+                timer3.Stop();
+            }
+             else
+                label7.Text = "Online";
+                timer2.Start();
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                checkBox1.Enabled = true;
+                checkBox2.Enabled = true;
+                checkBox3.Enabled = true;
+                check = Process.GetProcessesByName("ac_client");
+                button4.Hide();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int amount = int.Parse(textBox2.Text);
+            int LocalPlayer = vam.ReadInt32((IntPtr)0x00509B74);
+            int address = LocalPlayer + Ammo;
+            vam.WriteInt32((IntPtr)address, amount);
+            int address2 = LocalPlayer + GrenadeAmmo;
+            vam.WriteInt32((IntPtr)address2, amount);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int amount = int.Parse(textBox3.Text);
+            int LocalPlayer = vam.ReadInt32((IntPtr)0x00509B74);
+            int address = LocalPlayer + Armor;
+            vam.WriteInt32((IntPtr)address, amount);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            check = Process.GetProcessesByName("ac_client");
+            int LocalPlayer = vam.ReadInt32((IntPtr)0x00509B74);
+                int address1 = LocalPlayer + Health;
+                int address2 = LocalPlayer + Ammo;
+                int address3 = LocalPlayer + GrenadeAmmo;
+                int address4 = LocalPlayer + Armor;
+                if (checkBox1.Checked == true)
+                {
+                    textBox1.ReadOnly = true;
+                    button1.Enabled = false;
+                    vam.WriteInt32((IntPtr)address1, 9999);
+                }
+                if (checkBox2.Checked == true)
+                {
+                    textBox2.ReadOnly = true;
+                    button2.Enabled = false;
+                    vam.WriteInt32((IntPtr)address2, 9999);
+                    vam.WriteInt32((IntPtr)address3, 9999);
+                }
+                if (checkBox3.Checked == true)
+                {
+                    textBox3.ReadOnly = true;
+                    button3.Enabled = false;
+                    vam.WriteInt32((IntPtr)address4, 9999);
+                }
+                //----------------------------------------------------------------------\\
+                if (checkBox1.Checked == false)
+                {
+                    textBox1.ReadOnly = false;
+                    button1.Enabled = true;
+                }
+                if (checkBox2.Checked == false)
+                {
+                    textBox2.ReadOnly = false;
+                    button2.Enabled = true;
+                }
+                if (checkBox3.Checked == false)
+                {
+                    textBox3.ReadOnly = false;
+                    button3.Enabled = true;
+                }
+            }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (check.Length == 0)
+            {
+                timer1.Stop();
+                button4.Show();
+                label7.Text = "Offline";
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+                checkBox3.Enabled = false;
+            }
+            else
+                timer1.Start();
+            if (label7.Text == "Offline")
+            {
+                label7.ForeColor = Color.Red;
+            }
+            if (label7.Text == "Online")
+            {
+                label7.ForeColor = Color.Lime;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process[] check2 = Process.GetProcessesByName("ac_client");
+            if (check2.Length == 0)
+            {
+                MessageBox.Show("Game Not Detected...");
+            }
+            else
+                check = Process.GetProcessesByName("ac_client");
+            timer3.Start();
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            label8.ForeColor = Color.Red;
+            timer5.Start();
+            timer4.Stop();
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            label8.ForeColor = Color.Blue;
+            timer4.Start();
+            timer5.Stop();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
